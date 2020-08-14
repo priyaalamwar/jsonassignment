c748cd323facf4e39c5b2f96e751a331024e372c
priyaalamwar <alamwar.priya@gmail.com>
2020-08-14 23:47:41 +0530

    Update index.php
    Caps change

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once('jsonConvert.php');
 $flat_json          = file_get_contents(__DIR__ . '/flat.json');
 $flatJsonArr        = json_decode($flat_json, true);
 $hierarchicalJson   = Convert::convertJson($flatJsonArr, 'id', 'parent', 'children');
-print_R(json_encode($hierarchicalJson));
+print_r(json_encode($hierarchicalJson));
